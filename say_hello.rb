0b10971c9c48c8c87808374144092fb1997085ee
ohmyizzy <conneri19@barringtonschools.org>
2018-07-10 15:03:23 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,17 @@
 # Build your say_hello method here
+
+def say_hello(name= "Ruby Programmer")
+    puts "Hello #{name}!"
+end
+
+def practice(argument)
+  puts argument
+end
+
+practice("smile")
+
+say_hello
+say_hello("Izzy")
+
+
+# argument = "smile"
